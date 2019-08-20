58920c3fff72b0701ee0c1f2ffa93628b0af90ce
chetan-rc <47341116+chetan-rc@users.noreply.github.com>
2019-08-20 16:16:28 -0700

    udated i

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://34.227.17.255:8080/api/v1/";
+    this.baseUrl = "http://54.173.220.47:8080/api/v1/";
   }
   render(){
     return(
